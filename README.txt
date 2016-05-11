2e42fcd0dc0a75afed5c476fb4e0c80733625369
Winux503 <chaszam32@hotmail.co.uk>
2016-05-11 18:36:23 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+Made By Winux
+Part of the Winux Web Design Framework
+
+Feel free to use this code in any project as long as you credit me either on the site or in comments in the code.
+
+The Jquery API is indcluded
---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+	=========Credits=========
+	 Made By Samuel Tamplin
+	*/
+$(document).ready(function () {
+  $("#1").click(Pos);
+  $("#2").click(Pos);
+  $("#3").click(Pos);
+  function Pos(){
+	
+	if($(this).hasClass("pos")){
+		$(this).removeClass("pos");
+		$(this).addClass("img_block");
+		$(this).find("img").css("max-height","240px");
+		$(this).find(".img_text").css("font-size","100%");
+	}
+	else{
+		$(this).addClass("pos");
+		$(this).removeClass("img_block");
+		$(this).find("img").css("max-height","530px");
+		$(this).find(".img_text").css("font-size","50px");
+	}
+	
+  }
+  /*
+  Feel free to leach or 'fork' this code
+  Made by Samuel("Winux") Tamplin
+
+  |  | | |\ | |  | \_/
+  |/\| | | \| \__/ / \
+
+*/
+});
